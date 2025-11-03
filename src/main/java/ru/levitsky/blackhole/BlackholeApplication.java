62ee528acb6cc855576257726f65bca11bad1e75
Levitsky <lewickiy@gmail.com>
2025-11-04 02:49:01 +0300

    init commit

---
+++ src/main/java/ru/levitsky/blackhole/BlackholeApplication.java
@@ -0,0 +1,13 @@
+package ru.levitsky.blackhole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackholeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackholeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/levitsky/blackhole/BlackholeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.levitsky.blackhole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackholeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
