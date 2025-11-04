06eba44f8e180877fc957c5697c9c7bd593d05a3
Levitsky <lewickiy@gmail.com>
2025-11-04 22:56:50 +0300

    BlockService saveBlock and getBlockByHash created

--- src/main/java/ru/levitsky/blackhole/core/service/BlockService.java
+++ src/main/java/ru/levitsky/blackhole/core/service/BlockService.java
@@ -1,4 +1,27 @@
 package ru.levitsky.blackhole.core.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.levitsky.blackhole.repository.BlockRepository;
+import ru.levitsky.blackhole.singularity.entity.Block;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class BlockService {
+
+    private final BlockRepository blockRepository;
+
+    public Block saveBlock(Block block) {
+        if (block.getId() == null) {
+            block.setId(UUID.randomUUID());
+        }
+        return blockRepository.save(block);
+    }
+
+    public Optional<Block> getBlockByHash(String hash) {
+        return blockRepository.findByHash(hash);
+    }
 }
