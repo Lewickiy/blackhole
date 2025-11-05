9164d28014adfcd494291351578cc7eff5cbb0e1
Levitsky <lewickiy@gmail.com>
2025-11-06 02:53:42 +0300

    dtos validation added

--- src/main/java/ru/levitsky/blackhole/core/mapper/BlockMapper.java
+++ src/main/java/ru/levitsky/blackhole/core/mapper/BlockMapper.java
@@ -1,13 +1,13 @@
 package ru.levitsky.blackhole.core.mapper;
 
 import org.mapstruct.Mapper;
-import ru.levitsky.blackhole.eventhorizon.dto.BlockRequest;
 import ru.levitsky.blackhole.eventhorizon.dto.BlockResponse;
+import ru.levitsky.blackhole.eventhorizon.dto.BlockSaveRequest;
 import ru.levitsky.blackhole.singularity.entity.Block;
 
 @Mapper(componentModel = "spring")
 public interface BlockMapper {
     BlockResponse toResponse(Block block);
 
-    Block toEntity(BlockRequest request);
+    Block toEntity(BlockSaveRequest request);
 }
