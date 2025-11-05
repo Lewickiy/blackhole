7b502db5855521adfe4257fea7c711c537fb737e
Levitsky <lewickiy@gmail.com>
2025-11-06 02:19:33 +0300

    BlockMapper.java added with MapStruct

--- src/main/java/ru/levitsky/blackhole/core/mapper/BlockMapper.java
+++ src/main/java/ru/levitsky/blackhole/core/mapper/BlockMapper.java
@@ -1,4 +1,13 @@
 package ru.levitsky.blackhole.core.mapper;
 
-public class BlockMapper {
+import org.mapstruct.Mapper;
+import ru.levitsky.blackhole.eventhorizon.dto.BlockRequest;
+import ru.levitsky.blackhole.eventhorizon.dto.BlockResponse;
+import ru.levitsky.blackhole.singularity.entity.Block;
+
+@Mapper(componentModel = "spring")
+public interface BlockMapper {
+    BlockResponse entityToResponse(Block block);
+
+    Block requestToEntity(BlockRequest request);
 }
