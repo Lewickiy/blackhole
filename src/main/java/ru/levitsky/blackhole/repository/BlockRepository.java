77c476c2d3b09b41e12fa14db48c08627303c0ed
Levitsky <lewickiy@gmail.com>
2025-11-09 01:27:54 +0300

    feat(api): add repository method for batch block operations
    - Added repository method findAllByHashIn(List<String>) for batch hash lookup
    - Prepared base for batch deduplication API

--- src/main/java/ru/levitsky/blackhole/repository/BlockRepository.java
+++ src/main/java/ru/levitsky/blackhole/repository/BlockRepository.java
@@ -3,9 +3,11 @@ package ru.levitsky.blackhole.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.levitsky.blackhole.entity.Block;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BlockRepository extends JpaRepository<Block, UUID> {
+    List<Block> findAllByHashIn(List<String> hashes);
     Optional<Block> findByHash(String hash);
 }
